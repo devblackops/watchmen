ec4127f43db47c7ac49807092d51d0a6399751de
Brandon Olin <brandon@devblackops.io>
2016-06-08 20:51:25 -0700

    Update build scripts

--- build.ps1
+++ build.ps1
@@ -4,6 +4,9 @@ param(
 )
 
 Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+if (!(Get-Module -Name BuildHelpers -ListAvailable)) { Install-Module -Name BuildHelpers }
 if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake }
+
+Set-BuildEnvironment
 
 Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -nologo -Verbose:$VerbosePreference
