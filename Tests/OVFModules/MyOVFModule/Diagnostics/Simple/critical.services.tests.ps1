a33bbcc6898130ba42bcb8c6077db1bcd9171db2
Brandon Olin <brandon@devblackops.io>
2016-06-15 23:23:05 -0700

    Change tests

--- Tests/OVFModules/MyOVFModule/Diagnostics/Simple/critical.services.tests.ps1
+++ Tests/OVFModules/MyOVFModule/Diagnostics/Simple/critical.services.tests.ps1
@@ -13,11 +13,14 @@ describe 'Services' {
         service eventlog status { should be running }
 
         service mpssvc status { should be running }
+    }
 
+}
+
+describe 'More services' {
+    context 'Important Services' {
         service winrm status { should be running } 
 
         service schedule status { should be running }
-
     }
-
 }
