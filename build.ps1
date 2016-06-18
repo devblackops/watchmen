c9e7feb7d0ef1dbd74dd3cdc58d75875d361a5e4
Brandon Olin <brandon@devblackops.io>
2016-06-18 12:45:25 -0700

    Add Pester and PSScriptAnalyzer build tasks

--- build.ps1
+++ build.ps1
@@ -3,12 +3,53 @@ param(
     [string[]]$Task = 'default'
 )
 
+function Resolve-Module {
+    [Cmdletbinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]]$Name
+    )
+
+    Process {
+        foreach ($ModuleName in $Name) {
+            $Module = Get-Module -Name $ModuleName -ListAvailable
+            Write-Verbose -Message "Resolving Module [$($ModuleName)]"
+            
+            if ($Module) {
+                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery -Verbose:$false | 
+                    Measure-Object -Property Version -Maximum | 
+                    Select-Object -ExpandProperty Maximum
+
+                if ($Version -lt $GalleryVersion) {
+                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { 
+                        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+                    }
+                    
+                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+                    
+                    Install-Module -Name $ModuleName -Verbose:$false -Force
+                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $GalleryVersion
+                }
+                else {
+                    Write-Verbose -Message "Module Installed, Importing [$($ModuleName)]"
+                    Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+                }
+            }
+            else {
+                Write-Verbose -Message "[$($ModuleName)] Missing, installing Module"
+                Install-Module -Name $ModuleName -Verbose:$false -Force
+                Import-Module -Name $ModuleName -Verbose:$false -Force -RequiredVersion $Version
+            }
+        }
+    }
+}
+
 Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
 
-$reqModules = @('BuildHelpers', 'psake')
-Install-Module $reqmodules -Confirm:$false
-Import-Module $reqModules -Verbose:$false
+'BuildHelpers', 'psake' | Resolve-Module
 
 Set-BuildEnvironment
 
 Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -taskList $Task -nologo -Verbose:$VerbosePreference
+exit ( [int]( -not $psake.build_success ) )
--- psake.ps1
+++ psake.ps1
@@ -1,17 +1,32 @@
 
 properties {
-    
+    $sut = '.\Watchmen'
+    $tests = '.\Tests'
 }
 
-task default -depends prereqs
+task default -depends Prereqs, Analyze, Test
 
-task prereqs {
+task Prereqs {
     $modules = 'Pester', 'PSDeploy', 'PSScriptAnalyzer'
-    Install-Module $modules
+    Install-Module $modules -Confirm:$false
     Import-Module $modules -verbose:$false
 }
 
-task test
+task Analyze {
+    $saResults = Invoke-ScriptAnalyzer -Path $sut -Severity Error -Recurse -Verbose:$false
+    if ($saResults) {
+        $saResults | Format-Table
+        Write-Error -Message 'One or more Script Analyzer errors/warnings where found. Build cannot continue!'
+    }
+}
+
+task test {
+    $testResults = Invoke-Pester -Path $tests -PassThru
+    if ($testResults.FailedCount -gt 0) {
+        $testResults | Format-List
+        Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
+    }
+}
 
 task deploy
 
