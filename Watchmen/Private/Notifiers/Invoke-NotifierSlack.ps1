a2c8632b9a68a5a8d26efd4b53eb59ac58241597
Brandon Olin <brandon@devblackops.io>
2016-06-18 23:50:16 -0700

    Add Slack Notifier

--- Watchmen/Private/Notifiers/Invoke-NotifierSlack.ps1
+++ Watchmen/Private/Notifiers/Invoke-NotifierSlack.ps1
@@ -1,3 +1,72 @@
+#requires -modules PSSlack
+
 function Invoke-NotifierSlack {
-    
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory, ValueFromPipeline)]
+        [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'Watchmen.Notifier.Slack' })]
+        [pscustomobject]$Notifier,
+
+        [parameter(Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'OperationValidationResult' })]
+        [pscustomobject]$Results
+    )
+
+    if ($Notifier.Enabled) {
+        Import-Module -Name PSSlack -Verbose:$false -ErrorAction Stop
+
+        $o = ($Notifier | Format-Table -Property * -AutoSize | Out-String)
+        Write-Verbose -Message "Slack notifier called with options:`n$o" 
+        
+        $text = @"
+Watchmen reported a failure in OVF test:
+
+Computer: $env:COMPUTERNAME
+
+Module: $($results.Module)
+
+File: $($results.FileName)
+
+Descibe:  $($results.RawResult.Describe)
+
+Context: $($results.RawResult.Context)
+
+Test: $($results.RawResult.Name)
+
+Result: $($results.Result)
+
+Message: $($results.RawResult.FailureMessage)
+
+Duration: $($results.RawResult.Time.ToString())
+"@
+
+        $fields = @(
+            @{title = 'Computer'; value = $env:COMPUTERNAME }
+            @{title = 'Test'; value = $results.RawResult.Name }
+            @{title = 'Module'; value = $results.Module }
+            @{title = 'File'; value = $results.FileName }
+            @{title = 'Descibe'; value = $results.RawResult.Describe }
+            @{title = 'Context'; value = $results.RawResult.Context }    
+            @{title = 'Result'; value = $results.Result }
+            @{title = 'Message'; value = $results.RawResult.FailureMessage }
+            @{title = 'Duration'; value = $results.RawResult.Time.ToString() }
+        )
+
+        $params = @{
+            Color = [System.Drawing.Color]::red
+            Title = "Test details:"
+            TitleLink = $Notifier.TitleLink
+            Fields = $fields
+            PreText = "$($Notifer.IconEmoji) $($env:COMPUTERNAME) - $($results.Result.ToUpper()) - $($results.RawResult.Name)"
+            AuthorName = $Notifier.AuthorName
+            Fallback = $text
+        }
+        $att = New-SlackMessageAttachment @params
+        if ($Notifier.IconEmoji -ne [string]::Empty) {        
+            $msg = $att | New-SlackMessage -Channel $Notifer.Channel -IconEmoji $Notifier.IconEmoji
+        } else {
+            $msg = $att | New-SlackMessage -Channel $Notifer.Channel -IconUrl $Notifier.IconUrl
+        }    
+        $msg | Send-SlackMessage -Uri $Notifier.Token
+    }
 }
--- Watchmen/Public/Slack.ps1
+++ Watchmen/Public/Slack.ps1
@@ -1,23 +1,72 @@
-function Slack {    
+function Slack {
+    [cmdletbinding(DefaultParameterSetName = 'options')]
     param(
-        [parameter(Mandatory, Position = 0)]
-        [hashtable[]]$SlackConfig
-    )
+        [parameter(Mandatory, Position = 0, ParameterSetName = 'bool')]
+        [bool]$Enable,
 
+        [parameter(Mandatory, Position = 0, ParameterSetName = 'options')]
+        [hashtable[]]$Options
+    )
+    
     begin {
         Write-Debug -Message "Entering: $($PSCmdlet.MyInvocation.MyCommand.Name)"
         Assert-InWatchmen -Command $PSCmdlet.MyInvocation.MyCommand.Name
     }
-
+    
     process {
-        [pscustomobject]@{
+        $e = [pscustomobject]@{
             PSTypeName = 'Watchmen.Notifier.Slack'
             Type = 'Slack'
-            Values = $SlackConfig | % {
-                $_.PSTypeName = 'Watchmen.Notifier.Slack.Config'
-                [pscustomobject]$_
+            Title = ''
+            TitleLink = ''
+            Author = ''
+            AuthorIcon = ''
+            Token = ''
+            Channel = ''
+            IconUrl = ''
+            IconEmoji = ''
+            Enabled = $true
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq 'bool') {
+            # We were only passed a [bool] to enable/disable the Slack notifiaction so we're assuming
+            # somewhere before this we have specifed additional Slack parameters inside a WatchmenOptions
+            # block. Merge in those values
+
+            if ($global:Watchmen.Config.NotifierOptions.Slack) {
+                $e.Title = $global:Watchmen.Config.NotifierOptions.Slack.Title
+                $e.TitleLink = $global:Watchmen.Config.NotifierOptions.Slack.TitleLink
+                $e.Author = $global:Watchmen.Config.NotifierOptions.Slack.Author
+                $e.AuthorIcon = $global:Watchmen.Config.NotifierOptions.Slack.AuthorIcon
+                $e.Token = $global:Watchmen.Config.NotifierOptions.Slack.Token
+                $e.Channel = $global:Watchmen.Config.NotifierOptions.Slack.Channel
+                $e.IconUrl = $global:Watchmen.Config.NotifierOptions.Slack.IconUrl
+                $e.IconEmoji = $global:Watchmen.Config.NotifierOptions.Slack.IconEmoji
+                if (-not $Enable) {
+                    $e.Enabled = $false
+                } 
+            } else {
+                throw 'No Slack options have been specified in WatchmenOptions!'
+            }
+        } else {
+            Write-Debug -Message 'Slack options specified'
+            $e.Title = $Options.Title
+            $e.TitleLink = $Options.TitleLink
+            $e.Author = $Options.Author
+            $e.AuthorIcon = $Options.AuthorIcon
+            $e.Token = $Options.Token
+            $e.Channel = $Options.Channel
+            $e.IconUrl = $Options.IconUrl
+            $e.IconEmoji = $Options.IconEmoji
+
+            # If 'Slack' was called from inside WatchmenOptions, then persist these settings
+            # in the watchmen state for future reference
+            if ($global:Watchmen.InConfig) {
+                $global:Watchmen.Config.NotifierOptions.Slack = $e
             }
         }
+
+        return $e
     }
 
     end {
