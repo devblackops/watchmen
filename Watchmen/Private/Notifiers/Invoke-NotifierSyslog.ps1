e1c2e6592b9f300ac2ad7021a43721120462cf69
Brandon Olin <brandon@devblackops.io>
2016-06-19 23:26:55 -0700

    Write Watchmen test failure to syslog endpoint

--- Watchmen/Private/Notifiers/Invoke-NotifierSyslog.ps1
+++ Watchmen/Private/Notifiers/Invoke-NotifierSyslog.ps1
@@ -1,3 +1,29 @@
+#requires -modules Posh-SYSLOG
+
 function Invoke-NotifierSyslog {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory, ValueFromPipeline)]
+        [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'Watchmen.Notifier.Syslog' })]
+        [pscustomobject]$Notifier,
+
+        [parameter(Mandatory)]
+        [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'OperationValidationResult' })]
+        [pscustomobject]$Results
+    )
+
+    if ($Notifier.Enabled) {
+
+        Import-Module -Name Posh-SYSLOG -Verbose:$false -ErrorAction Stop
+
+        $o = ($Notifier | Format-Table -Property * -AutoSize | Out-String)
+        Write-Verbose -Message "Syslog notifier called with options:`n$o"
     
+        $msg = "$($Results.RawResult.Describe) -> $($Results.RawResult.Context) -> $($Results.RawResult.Name) -> $($Results.RawResult.Result.ToUpper())"
+
+        foreach ($endpoint in $Notifier.Endpoint) {
+            Send-SyslogMessage -Server $endpoint -Message $msg -Severity 'Critical' -Facility 'logalert'
+        }
+        
+    }
 }
--- Watchmen/Public/Syslog.ps1
+++ Watchmen/Public/Syslog.ps1
@@ -1,7 +1,11 @@
-function Syslog {    
+function Syslog {
+    [cmdletbinding(DefaultParameterSetName = 'endpoint')]
     param(
-        [parameter(Mandatory, Position = 0)]
-        [string[]]$Endpoints
+        [parameter(Mandatory, Position = 0, ParameterSetName = 'endpoint')]
+        [string[]]$Endpoints,
+
+        [parameter(Mandatory, Position = 0, ParameterSetName = 'bool')]
+        [bool]$Enable
     )
 
     begin {
@@ -10,11 +14,38 @@ function Syslog {
     }
 
     process {
-        [pscustomobject]@{
+        $e = [pscustomobject]@{
             PSTypeName = 'Watchmen.Notifier.Syslog'
             Type = 'Syslog'
-            Values = $Endpoints
+            Endpoint = @()
+            Enabled = $true
         }
+
+        if ($PSCmdlet.ParameterSetName -eq 'bool') {
+            # We were only passed a [bool] to enable/disable the syslog notifiaction so we're assuming
+            # somewhere before this we have specifed additional syslog parameters inside a WatchmenOptions
+            # block. Merge in those values
+
+            if ($global:Watchmen.Config.NotifierOpsions.Syslog) {
+                $e.Endpoint = $global:Watchmen.Config.NotifierOptions.Syslog.Endpoint
+                if (-not $Enable) {
+                    $e.Enabled = $false
+                }
+            } else {
+                throw 'No syslog options have been specified in WatchmenOptions!'
+            }
+        } else {
+            Write-Debug -Message 'endpoint options specified'
+            $e.Endpoint += $Endpoints
+            
+            # If 'Syslog' was called from inside WatchmenOptions, then persist these settings
+            # in the watchmen state for future reference
+            if ($global:Watchmen.InConfig) {
+                $global:Watchmen.Config.NotifierOptions.Syslog = $e
+            }
+        }
+
+        return $e
     }
 
     end {
