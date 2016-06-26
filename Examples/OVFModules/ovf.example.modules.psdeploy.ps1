6b8151224094466f057ef50435a9fffe5fba2f45
Brandon Olin <brandon@devblackops.io>
2016-06-25 23:05:30 -0700

    Update example OVF modules

---
+++ Examples/OVFModules/ovf.example.modules.psdeploy.ps1
@@ -0,0 +1,12 @@
+Deploy My-OVF-Modules {
+
+    By FileSystem OVF.Example1 {
+        FromSource '.\OVF.Example1\'
+        To 'C:\Program Files\WindowsPowerShell\Modules\OVF.Example1'
+    }
+
+    By FileSystem OVF.Example2 {
+        FromSource '.\OVF.Example2\'
+        To 'C:\Program Files\WindowsPowerShell\Modules\OVF.Example2'
+    }
+}
