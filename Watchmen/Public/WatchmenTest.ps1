e6312c84dc91dead1e2c60607fd014969bb76165
Brandon Olin <brandon@devblackops.io>
2016-07-02 22:25:54 -0700

    Rename FileSystem notifier to LogFile

--- Watchmen/Public/WatchmenTest.ps1
+++ Watchmen/Public/WatchmenTest.ps1
@@ -29,7 +29,7 @@ function WatchmenTest {
             Notifiers = @{
                 Email = @()
                 EventLog = @()
-                FileSystem = @()
+                LogFile = @()
                 Slack = @()
                 Syslog = @()
             }
