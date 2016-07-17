0d74b2bcbe175e58c0cbe490d4c820b3edac545c
Brandon Olin <brandon@devblackops.io>
2016-07-17 15:40:25 -0700

    Use environment variable for Program Files path

--- TestArtifacts/OVFModules/ovf.example.modules.psdeploy.ps1
+++ TestArtifacts/OVFModules/ovf.example.modules.psdeploy.ps1
@@ -2,11 +2,11 @@ Deploy My-OVF-Modules {
 
     By FileSystem OVF.Example1 {
         FromSource '.\OVF.Example1\'
-        To 'C:\Program Files\WindowsPowerShell\Modules\OVF.Example1'
+        To "$env:programfiles\WindowsPowerShell\Modules\OVF.Example1"
     }
 
     By FileSystem OVF.Example2 {
         FromSource '.\OVF.Example2\'
-        To 'C:\Program Files\WindowsPowerShell\Modules\OVF.Example2'
+        To "$env:programfiles\WindowsPowerShell\Modules\OVF.Example2"
     }
 }
