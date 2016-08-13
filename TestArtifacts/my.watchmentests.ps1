67c4eeb6fdb55f03d0157a82954bc3cecf355e53
Brandon Olin <brandon@devblackops.io>
2016-08-12 21:53:58 -0700

    Update test file

--- TestArtifacts/my.watchmentests.ps1
+++ TestArtifacts/my.watchmentests.ps1
@@ -10,9 +10,28 @@ WatchmenOptions {
             eventtype = 'warning'
         }
         logfile 'c:\temp\watchmen.log'
+        logfile @(
+            'c:\temp\watchmen2.log',
+            'c:\temp\watchmen3.log'
+        )
         syslog 'localhost'
+        syslog @(
+            'endpoint1.mydomain.tld',
+            'endpoint2.mydomain.tld'
+        )
         powershell {
-            Write-Host $args[0]
+            Write-Host ("There was an error! $args[0]")
+        }
+        powershell {
+            '.\notify.ps1'
+        }
+        slack @{
+            Token = $env:WATCHMEN_SLACK_WEBHOOK
+            Channel = '#Watchmen_Alerts'
+            TitleLink = 'www.google.com'
+            AuthorName = $env:COMPUTERNAME
+            PreText = 'Everything is on :fire:'
+            IconEmoji = ':fire:'
         }
     }
 }
@@ -20,6 +39,12 @@ WatchmenOptions {
 # This test should PASS
 WatchmenTest 'OVF.Example1' {
     version 0.1.0
+    test 'Storage Capacity'
+    notifies {
+        syslog @(
+            '1.2.3.4', 'syslog.mydomain.tld'
+        )
+    }
 }
 
 # This test should FAIL and call (2) additional notifiers
@@ -28,14 +53,16 @@ WatchmenTest 'OVF.Example1' {
     parameters @{
         FreeSystemDriveThreshold = 50000
     }        
-    # notifies {
-    #     logfile 'c:\temp\watchmen2.log'
-    #     powershell '.\notify.ps1'
-    # }
+    notifies {
+        logfile 'c:\temp\watchmen2.log'
+        powershell '.\notify.ps1'
+    }
 }
 
 # Run tests with defaults for everything
-WatchmenTest 'OVF.Example2' {}
+WatchmenTest 'OVF.Example2' {
+    testtype 'all'
+}
 
 # This test should FAIL because module is missing
 # WatchmenTest 'MyOVFModule' {
